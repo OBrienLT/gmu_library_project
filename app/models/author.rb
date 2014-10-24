705156d201d95f44b5b954cf038091896558d484
OBrienLT <liam.t.obrien@hotmail.com>
2014-10-23 20:30:41 -0400

    Added Author validations perslide 23: presence of nationality, biography, image_url and minimum length of biography.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
-	validates :name, :dob, presence: true
+	validates :name, :dob, :nationality, :biography, :image_url, presence: true
+	validates :biography, length: { minimum: 15, too_short: "the author's biography is too short." }
 	has_many :books, dependent: :destroy
 end
